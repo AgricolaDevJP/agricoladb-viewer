53c96d41496973538478c269785a48bab3ca84f1
ASAKURA Kazuki <32762324+Arthur1@users.noreply.github.com>
2024-01-09 22:38:35 +0900

    purge css

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,7 @@
 import partytown from '@astrojs/partytown'
 import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
+import purgecss from 'astro-purgecss'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
@@ -12,6 +13,9 @@ export default defineConfig({
       config: {
         forward: ['dataLayer.push'],
       },
+    }),
+    purgecss({
+      safelist: ['btn-info'],
     }),
   ],
   vite: {
